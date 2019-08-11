cde22e3c0adb79627b3e309be1324800edba4354
jaceyi <yijinchun@outlook.com>
2019-08-11 13:08:48 +0800

    fix not title error

--- _site/js/util.js
+++ _site/js/util.js
@@ -1,17 +1,17 @@
 function addClass(el, className) {
-        var _ = el.className.split(' ');
-        var index = _.indexOf(className);
-        if (index >= 0) return className;
-        _.push(className);
-        el.className = _.join(' ');
-    }
+  var _ = el.className.split(' ');
+  var index = _.indexOf(className);
+  if (index >= 0) return className;
+  _.push(className);
+  el.className = _.join(' ');
+}
 
 function removeClass(el, className) {
-    var _ = el.className.split(' ');
-    var index = _.indexOf(className);
-    if (index < 0) return className;
-    _.splice(index, 1);
-    el.className = _.join(' ');
+  var _ = el.className.split(' ');
+  var index = _.indexOf(className);
+  if (index < 0) return className;
+  _.splice(index, 1);
+  el.className = _.join(' ');
 }
 
 function getElementTop(el) {
