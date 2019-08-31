65a522afafef4b2874b01bd35a5ddd496ea48a47
jaceyi <yijinchun@outlook.com>
2019-08-31 18:46:44 +0800

    add blog

--- js/util.js
+++ js/util.js
@@ -1,17 +1,9 @@
 function addClass(el, className) {
-  var _ = el.className.split(' ');
-  var index = _.indexOf(className);
-  if (index >= 0) return className;
-  _.push(className);
-  el.className = _.join(' ');
+  el.classList.add(className);
 }
 
 function removeClass(el, className) {
-  var _ = el.className.split(' ');
-  var index = _.indexOf(className);
-  if (index < 0) return className;
-  _.splice(index, 1);
-  el.className = _.join(' ');
+  el.classList.remove(className);
 }
 
 function getElementTop(el) {
